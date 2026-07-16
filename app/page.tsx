2f7503f3a19aa26036ca481c7a633181320548b3
valerio <valeriofusco1109v@gmail.com>
2026-07-16 20:45:51 +0200

    added suspense and skeleton loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { CveResults } from "@/components/cve-results";
+import { CveResultsSkeleton } from "@/components/cve-results-skeleton";
 import { mapCveReportToFindings } from "@/lib/cve";
 import type { CveApiEntry } from "@/lib/types";
+import { Suspense } from "react";
 
 export default async function Home() {
   const data = await fetch("https://dummyjson.com/c/6b25-ad69-4222-b625");
@@ -10,7 +12,9 @@ export default async function Home() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center bg-background font-sans text-foreground">
       <main className="w-full max-w-6xl space-y-4 bg-background p-8">
-        <CveResults findings={findings} />
+        <Suspense fallback={<CveResultsSkeleton />}>
+          <CveResults findings={findings} />
+        </Suspense>
       </main>
     </div>
   );
